a9eb656dc5fd981e78878528a4278a644c87d6e7
Roman Himadinov <roman@brandetize.com>
2018-03-10 19:22:51 -0800

    Added demo page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+var emailArr = [
+  "dist/gd_template_margarita.html",
+  "dist/gd_template_org_letter.html", 
+  "dist/getdoctor_hm_letter.html"
+];
+for (let i = 0; i < emailArr.length; i++) {
+  $(".email-" + i).click(function () {
+    $("#email-page").attr("include-html", emailArr[i]);
+    includeHTML();
+  });
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
 var fullPageModal = (function ($) {
   var fpm = {};
 
